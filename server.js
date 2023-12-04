bd56b8fcd0f78dac1e5205093f5b4bb1d2bf97d5
mite <mohammadh.tay@gmail.com>
2023-12-04 14:49:17 -0500

    nocookie

--- server.js
+++ server.js
@@ -30,13 +30,7 @@ const connect = async () => {
 }
 app.use(cors({ origin: "https://dmetronic.netlify.app", credentials: true }));
 app.use(express.json());
-app.use(cookieParser({
-    // secure:false
-    signed: false,
-    secure: true,
-    httpOnly: false,
-    sameSite: 'none'
-}));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
